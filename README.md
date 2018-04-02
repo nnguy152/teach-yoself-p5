64548be7c714e822393b54286885d09c1a718ba1
Nhi Nguyen <nnguy152@gmail.com>
2018-04-02 16:32:02 -0400

    .

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # p5.js
 This is a quick teach-myself demo of p5.js.
-p5.js is a Javascript library with drawing functionality that allows you to use a browser page as a sketchpad.
+p5.js is a Javascript library with drawing functionality that allows you to use a browser page as a sketchpad. It can be used for art, data visualization, and websites alongside other tools like HTML5, CSS and JavaScript.
 
 # Why p5.js?
 I am a self-proclaimed artist and I consider styling of a webpage or application or product (or anything really) to be very important. If something isn't clean (or messy in a clean way), it decreases user trust in the application and can decrease the overall value of the product.
@@ -9,3 +9,9 @@ p5.js looks like a useful tool to create artsy user interfaces.
 
 # Download
 Open up your local repository in your CLI and typed "npm install -s p5!
+
+# Documentation
+https://p5js.org/reference/
+
+I followed this matrix tutorial by Emily Xie to make this practice project.
+https://www.youtube.com/watch?v=S1TQCi9axzg
--- sketch.js
+++ sketch.js
@@ -1,7 +1,68 @@
-function setup() {
-   // put setup code here
+var streams = []
+var symbolSize = 45
+
+// setUp to set up the canvas size, start color if needed, etc
+// p5 always needs setup and draw function
+setup = () => {
+   createCanvas(990, 500)
+
+   var x = 0
+   for (var i = 0; i <= width / symbolSize; i++) {
+      var stream = new Stream()
+      stream.generateSymbols(x, random(-2000, 0))
+      streams.push(stream)
+      x += symbolSize
+   }
+   textSize(symbolSize)
 }
 
-function draw() {
-   // put drawing code here
+// renders the function art from below
+draw = () => {
+   background(255)
+   streams.forEach(stream => {
+      stream.render()
+   })
+}
+
+
+function Symbol(x, y, speed) {
+   this.x = x
+   this.y = y
+   this.value = '\u{1F4A9}'
+
+   this.speed = speed
+
+   this.poopEmoji = () => {
+      this.value
+   }
+
+   this.rain = () => {
+      this.y = (this.y >= height) ? 0 : this.y += this.speed
+   }
+
+}
+
+function Stream() {
+   this.symbols = []
+   this.speed = random(1, 10)
+
+   this.generateSymbols = function (x, y) {
+      for (var i = 0; i <= 1; i++) {
+         symbol = new Symbol(
+            x,
+            y,
+            this.speed
+         )
+         symbol.poopEmoji()
+         this.symbols.push(symbol)
+      }
+   }
+
+   this.render = () => {
+      this.symbols.forEach(symbol => {
+         text(symbol.value, symbol.x, symbol.y)
+         symbol.rain()
+         symbol.poopEmoji()
+      })
+   }
 }
